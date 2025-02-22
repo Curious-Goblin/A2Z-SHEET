c9b5130af74146efc4b1b7719bc7c3da46735cdf
Curious-Goblin <poddarsourabh9939@gmail.com>
2025-02-22 22:54:48 +0530

    22/02/25

--- greedy algorithms/medium problems/nMeetingsInRoom.cpp
+++ greedy algorithms/medium problems/nMeetingsInRoom.cpp
@@ -7,7 +7,27 @@ class Solution
 public:
     int maxMeetings(vector<int> &start, vector<int> &end)
     {
-        int i=0,j=0;
+        if (start.size() == 1)
+        {
+            return 1;
+        }
+        int meetEnd = 0, meetings = 1;
+        vector<pair<int, int>> meets;
+        for (int i = 0; i < start.size(); i++)
+        {
+            meets.push_back({end[i], start[i]});
+        }
+        sort(meets.begin(), meets.end());
+        meetEnd = meets[0].first;
+        for (int i = 0; i < meets.size(); i++)
+        {
+            if (meets[i].second > meetEnd)
+            {
+                meetings++;
+                meetEnd = meets[i].first;
+            }
+        }
+        return meetings;
     }
 };
 
@@ -24,7 +44,11 @@ int main()
     {
         cin >> end[i];
     }
+    // for(int i=0;i<n;i++){
+    //     cout<<start[i]<<" ";
+    // }
+    // cout<<endl;
     Solution solve;
-    cout<<solve.maxMeetings(start,end);
+    cout << solve.maxMeetings(start, end);
     return 0;
 }
